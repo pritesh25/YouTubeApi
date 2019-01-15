b92b6cc299ed68e0bf08d49918ab4571f7d1e8eb
pritesh <priteshvishwakarma@gmail.com>
2019-01-15 10:49:50 +0530

    Initial commit

---
+++ app/src/main/java/com/herba/sdk/youtubeapi/MainActivity.java
@@ -0,0 +1,20 @@
+package com.herba.sdk.youtubeapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //OAuth Client
+    //456285709469-pfovu7ui2sf58crrks48qflbpijuaijp.apps.googleusercontent.com
+
+    //API Key
+    //AIzaSyDmSScmN_Lf2uAlncJWziTyzM6Om59irRs
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
